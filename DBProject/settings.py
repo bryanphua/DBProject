25bc8e4a2b74bd0776965f8a98821fe45fa2ac79
Ruth <ruthwongny@gmail.com>
2017-11-13 09:03:00 +0800

    added aws database by bryan

--- DBProject/settings.py
+++ DBProject/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'DBProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'USER': 'member',
+        'PASSWORD': 'password098',
+        'HOST': 'database-project.crqw7gce6pgf.ap-southeast-1.rds.amazonaws.com',
+        'PORT': '3306'
     }
 }
 
