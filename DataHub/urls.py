156b3894402dc795e0a3ab8041cc8d1e1b366623
Ruth <ruthwongny@gmail.com>
2017-11-21 09:18:19 +0800

    sign out function

--- DataHub/urls.py
+++ DataHub/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     url(r'^new/$', views.new_dataset, name = 'new'),
     url(r'^signup/$', views.sign_up, name = 'sign_up'),
     url(r'^signin/$', views.sign_in, name = 'sign_in'),
+    url(r'^signout/$', views.sign_out, name = 'sign_out'),
 ]
--- DataHub/views.py
+++ DataHub/views.py
@@ -1,31 +1,58 @@
 from django.shortcuts import render, redirect
 from django.db import connection, IntegrityError
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
-    context = {}
+    context = { 'auth': False }
     if request.user.is_authenticated:
-        context = { 'user': request.user.id }
+        context = {
+            'auth': True,
+            'id': request.user.id,
+            'username': request.user.username
+        }
     return render(request, 'index.html', context)
 
 def profile(request):
-    context = {}
+    if not request.user.is_authenticated:
+        messages.info(request, 'Please login to view your profile')
+        return redirect('/')
+    context = {
+        'auth': True,
+        'id': request.user.id,
+        'username': request.user.username
+    }
     return render(request, 'profile.html', context)
 
 def dataset(request):
-    context = {}
+    if not request.user.is_authenticated:
+        context = {}
+    else:
+        context = {
+            'auth': True,
+            'id': request.user.id,
+            'username': request.user.username
+        }
     return render(request, 'dataset.html', context)
 
 def new_dataset(request):
-    context = {}
+    if not request.user.is_authenticated:
+        messages.info(request, 'Please login to view your profile')
+        return redirect('/')
+    context = {
+        'auth': True,
+        'id': request.user.id,
+        'username': request.user.username
+    }
     return render(request, 'create_dataset.html', context)
 
 def sign_up(request):
+    if request.user.is_authenticated:
+        return redirect('/')
     context = {}
     if request.method == 'GET':
-        context = {}
         return render(request, 'sign_up.html', context)
     elif request.method == 'POST':
         params = request.POST
@@ -37,6 +64,8 @@ def sign_up(request):
         return redirect('/')
 
 def sign_in(request):
+    if request.user.is_authenticated:
+        return redirect('/')
     context = {}
     if request.method == 'GET':
         return render(request, 'sign_in.html', context)
@@ -46,7 +75,13 @@ def sign_in(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, 'Login success')
             return redirect('/')
         else:
             context = { 'error': 'Invalid login credentials' }
             return render(request, 'sign_in.html', context)
+
+def sign_out(request):
+    logout(request)
+    messages.success(request, 'You have signed out successfully')
+    return redirect('/')
