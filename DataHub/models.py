6c3d1f7b70f98e25ae910b0bb05ce1299772e1c5
Ruth <ruthwongny@gmail.com>
2017-11-23 08:28:20 +0800

    added dataset_list model

--- DataHub/models.py
+++ DataHub/models.py
@@ -2,7 +2,14 @@ from ModelClass.ModelClass import Model,Column
 
 class auth_user(Model):
     id = Column('id','int',unique=True)
-    password = Column('password','char')
-    email = Column('email','char',unique=True)
-    username = Column('username','char')
+    password = Column('password','char', not_null=True)
+    email = Column('email','char',unique=True, not_null=True)
+    username = Column('username','char', unique=True, not_null=True)
+    first_name = Column('first_name', 'char', not_null=True)
     is_staff = Column('is_staff','int')
+
+class dataset_list(Model):
+    id = Column('id', 'int', unique=True)
+    name = Column('name', 'char')
+    creator_user_id = Column('creator_user_id', 'int', not_null=True)
+    endorsed_by = Column('endorsed_by', 'char', )
